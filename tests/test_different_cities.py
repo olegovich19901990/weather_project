e59c53019295a9177562fc47dcb57d4fc8db3c98
Твоє ім’я на GitHub <dobrodii2017@gmail.com>
2025-08-20 14:32:30 +0300

    new_test_coordinates

---
+++ tests/test_different_cities.py
@@ -0,0 +1,29 @@
+import pytest
+from weather_app.weather import get_forecast_five_units
+from weather_app.weather import get_pollution
+
+
+test_cities = [
+    ("Kyiv", (50.450001, 30.523333)),
+    ("London", (51.507351, -0.127758)),
+    ("Tokyo", (35.689487, 139.691711)),
+]
+
+@pytest.mark.parametrize("city,coordinates", test_cities)
+def test_hourly_five(coordinates):
+    lat, lon = coordinates
+    response = get_forecast_five(lat, lon)
+    assert response.status_code == 200, f"Failed for {city}"
+
+@pytest.mark.parametrize("city,coordinates", test_cities)
+def test_hourly_five_units(city, coordinates):
+    lat, lon = coordinates
+    response = get_forecast_five_units(lat, lon)
+    assert response.status_code == 200, f"Failed for {city}"
+
+@pytest.mark.parametrize("city,coordinates", test_cities)
+def test_pollution(city, coordinates):
+    lat, lon = coordinates
+    response = get_pollution(lat, lon)
+    assert response.status_code == 200, f"Failed for {city}"
+    assert response.json() is not None
