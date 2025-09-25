e9149ae404c36d563d546682a07f22e4dc541620
Твоє ім’я на GitHub <dobrodii2017@gmail.com>
2025-09-25 17:42:47 +0300

    fixed test_weather_api.py and test_geocoding.py

--- weather_app/weather.py
+++ weather_app/weather.py
@@ -37,3 +37,32 @@ def get_geocoding_zip(zip_code, country_code):
 def get_geocoding_coordinates(lat, lon):
     url = f"http://api.openweathermap.org/geo/1.0/reverse?lat={lat}&lon={lon}&limit=1&appid={API_KEY}"
     return requests.get(url)
+
+def get_forecast_climate(lat, lon):
+    url = f"http://api.openweathermap.org/data/2.5/forecast/climate?lat={lat}&lon={lon}&appid={API_KEY}"
+    return requests.get(url)
+
+def get_forecast_climate_city(city, country_code):
+    #https://pro.openweathermap.org/data/2.5/forecast/climate?q={city name},{country code}&appid={API key}
+    url = f"http://api.openweathermap.org/data/2.5/forecast/climate?q={city},{country_code}&appid={API_KEY}"
+    return requests.get(url)
+
+def get_forecast_climate_xml(city, country_code):
+    #api.openweathermap.org/data/2.5/forecast/climate?q=London&mode=xml
+    url = f"http://api.openweathermap.org/data/2.5/forecast/climate?q={city},{country_code}&appid={API_KEY}&mode=xml"
+    return requests.get(url)
+
+def get_forecast_climate_cnt_city(city, country_code, cnt):
+    #api.openweathermap.org/data/2.5/forecast/climate?q=London&cnt=7
+    url = f"http://api.openweathermap.org/data/2.5/forecast/climate?q={city},{country_code}&appid={API_KEY}&cnt={cnt}"
+    return requests.get(url)
+
+def get_forecast_climate_units(city, country_code, unit):
+    #api.openweathermap.org/data/2.5/forecast/climate?q=London&units=metric
+    url = f"http://api.openweathermap.org/data/2.5/forecast/climate?q={city},{country_code}&appid={API_KEY}&units={unit}"
+    return requests.get(url)
+
+def get_forecast_climate_lang(city, country_code, lang):
+    #api.openweathermap.org/data/2.5/forecast/climate?q=London&lang=ua
+    url = f"http://api.openweathermap.org/data/2.5/forecast/climate?q={city},{country_code}&appid={API_KEY}&lang={lang}"
+    return requests.get(url)
